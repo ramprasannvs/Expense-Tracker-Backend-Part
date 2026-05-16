a8bd7d1f3852ea08b218b9d2fd4973de0c01366e
Ram Prasann <106461823+ramprasannvs@users.noreply.github.com>
2026-05-16 23:42:05 +0530

    update

--- utils/email.js
+++ utils/email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
